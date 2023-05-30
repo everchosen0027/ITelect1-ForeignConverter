60f781bc9181b8d95f31de44102d18b1ca738277
everchosen0027 <lance.tagadan@lccdo.edu.ph>
2023-05-30 15:13:13 +0800

    converter commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from "../services/data.service";
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,63 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  symbols: any;
+  codeCur: any = '';
+  descCur: any;
+  seleCur: any;
+  convData: any = [];
+  ratesData: any = [];
+  curAmount: any = '1.00';
 
-  constructor() {}
+  constructor(
+    private dataServer: DataService,
+    public loader: LoadingController) {}
+
+  ngOnInit() {
+    this.getSymbols();
+  }
+
+  async getSymbols() {
+    await this.dataServer.getSymbols().then((data: any) => {
+      this.symbols = [];
+      for (var key in data.symbols) {
+        if (data.symbols.hasOwnProperty(key)) {
+          this.symbols.push({ "code": data.symbols[key].code, "desc": data.symbols[key].description });
+        }
+      }
+    });
+  }
+
+  async convertCurrency(base, amount) {
+    await this.dataServer.convCur(base, amount).then((data: any) => {
+      this.ratesData = Object.keys(data.rates)
+        .map(key => ({ name: key, value: data.rates[key] }))
+    });
+    this.convData = [];
+    for (let i = 0; i < this.symbols.length; i++) {
+      this.convData.push({
+        ...this.symbols[i],
+        ...(this.ratesData.find((itmInner) => itmInner.name === this.symbols[i].code))
+      }
+      );
+    }
+  }
+
+  async getRates() {
+    const loading = await this.loader.create({
+      message: 'Changing the currency',
+      duration: 2000
+    });
+    await loading.present();
+    await this.convertCurrency(this.codeCur, this.curAmount);
+  }
+
+  selectedCur(ev) {
+    let selCur = ev.detail.value;
+    let a = selCur.split("|");
+    this.codeCur = a[0];
+    this.descCur = a[1];
+    this.seleCur = a[0] + ' - ' + a[1];
+  }
 
 }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  symbols = 'https://api.exchangerate.host/symbols';
+  rates = 'https://api.exchangerate.host/latest?base=';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+  constructor(private httpClient: HttpClient) { }
+
+  getSymbols() {
+    return new Promise(resolve => {
+      this.httpClient.get(this.symbols, this.httpOptions).subscribe((data: any) => {
+        resolve(data);
+        console.log(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  convCur(base, amount) {
+    return new Promise(resolve => {
+      let newRates = this.rates + base + '&amount=' + amount;
+      this.httpClient.get(newRates, this.httpOptions).subscribe((data: any) => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+}
